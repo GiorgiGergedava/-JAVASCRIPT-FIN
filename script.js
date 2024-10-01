7d13dcffe341ac19a8f5cb73c6032051d1f4bbf2
GiorgiGergedava <giogergedava123@gmail.com>
2024-10-01 23:21:58 +0300

    add,style,script

--- script.js
+++ script.js
@@ -1,84 +1,215 @@
+// (use strict)
+
+// server
+
+// const http = require('http')
+// const server = http.createServer(function (req, res) {
+//     console.log(req);
+// })
+// const port = 5501;
+// server.listen(port, function () {
+//     console.log("server running on " + port);
+// })
+
+
+
 $(document).ready(function () {
-    $(window).scroll(function () {
-        // sticky navbar on scroll script
-        if (this.scrollY > 20) {
-            $('.navbar').addClass("sticky");
-        } else {
-            $('.navbar').removeClass("sticky");
-        }
+  $(window).scroll(function () {
+    // sticky navbar on scroll script
+    if (this.scrollY > 20) {
+      $('.navbar').addClass("sticky");
+    } else {
+      $('.navbar').removeClass("sticky");
+    }
 
-        // scroll-up button show/hide script
-        if (this.scrollY > 500) {
-            $('.scroll-up-btn').addClass("show");
-        } else {
-            $('.scroll-up-btn').removeClass("show");
-        }
-    });
+    // scroll-up button show/hide script
+    if (this.scrollY > 500) {
+      $('.scroll-up-btn').addClass("show");
+    } else {
+      $('.scroll-up-btn').removeClass("show");
+    }
+  });
 
-    // slide-up script
-    $('.scroll-up-btn').click(function () {
-        $('html').animate({ scrollTop: 0 });
-        // removing smooth scroll on slide-up button click
-        $('html').css("scrollBehavior", "auto");
-    });
+  // slide-up script
+  $('.scroll-up-btn').click(function () {
+    $('html').animate({ scrollTop: 0 });
+    // removing smooth scroll on slide-up button click
+    $('html').css("scrollBehavior", "auto");
+  });
 
-    $('.navbar .menu li a').click(function () {
-        // applying again smooth scroll on menu items click
-        $('html').css("scrollBehavior", "smooth");
-    });
+  $('.navbar .menu li a').click(function () {
+    // applying again smooth scroll on menu items click
+    $('html').css("scrollBehavior", "smooth");
+  });
 
-    // toggle menu/navbar script
-    $('.menu-btn').click(function () {
-        $('.navbar .menu').toggleClass("active");
-        $('.menu-btn i').toggleClass("active");
-    });
+  // toggle menu/navbar script
 
-    // typing text animation script
-    var typed = new Typed(".typing", {
-        strings: [ "Front End Developer", "Junior", "Front End Developer",],
-        typeSpeed: 100,
-        backSpeed: 80,
-        i: 0
-        
+ 
+
+  $('.menu-btn').click(function () {
+    $('.navbar .menu').toggleClass("active");
+    $('.menu-btn i').toggleClass("active");
+  });
+
+  // scroll bar
+  var preogressbar = document.getElementById("preogressbar")
+  var percent = document.getElementById("percent")
+
+  var totalHeight = document.body.scrollHeight - window.innerHeight;
+  window.onscroll = function () {
+    var progress = (window.pageYOffset / totalHeight) * 100;
+    preogressbar.style.height = progress + "%";
+    percent.innerHTML = "Page Scrolled " + Math.round(progress) + "%"
+  }
+
+  // typing text animation script
+  var typed = new Typed(".typing", {
+    strings: ["Front End Developer", "Junior", "Front End Developer",],
+    typeSpeed: 100,
+    backSpeed: 80,
+    i: 0
 
 
-    });
+
+  });
 
 
-    var typed = new Typed(".typing-2", {
-        strings: ["Front End Developer",],
-        typeSpeed: 100,
-        backSpeed: 60,
+  var typed = new Typed(".typing-2", {
+    strings: ["Front End Developer",],
+    typeSpeed: 100,
+    backSpeed: 60,
 
-    });
+  });
 
-    var typed = new Typed(".typing-3", {
-        strings: ["Connect with me on :)"],
-        typeSpeed: 100,
-        backSpeed: 60,
+  var typed = new Typed(".typing-3", {
+    strings: ["Connect with me on :)"],
+    typeSpeed: 100,
+    backSpeed: 60,
 
-    });
+  });
 
-    // owl carousel script
-    $('.carousel').owlCarousel({
-        margin: 20,
-        loop: true,
-        autoplay: true,
-        autoplayTimeOut: 2000,
-        autoplayHoverPause: true,
-        responsive: {
-            0: {
-                items: 1,
-                nav: false
-            },
-            600: {
-                items: 2,
-                nav: false
-            },
-            1000: {
-                items: 3,
-                nav: false
-            }
-        }
-    });
+  // owl carousel script
+  $('.carousel').owlCarousel({
+    margin: 20,
+    loop: true,
+    autoplay: true,
+    autoplayTimeOut: 2000,
+    autoplayHoverPause: true,
+    responsive: {
+      0: {
+        items: 1,
+        nav: false
+      },
+      600: {
+        items: 2,
+        nav: false
+      },
+      1000: {
+        items: 3,
+        nav: false
+      }
+    }
+  });
 });
+
+
+// form validation
+
+const formE1 = document.getElementById('formElement');
+
+formEl.addEventListener("submit", function (e) {
+  e.preventDefault();
+  let errors = {};
+})
+
+
+// username
+const usernameValue = document.getElementById("usernameField").value;
+
+if (usernameValue == "") {
+  errors.username = "Username Field can not be empty";
+}
+
+
+// useremail
+const emailValue = document.getElementById("emailField").value;
+
+if (emailValue == "") {
+  errors.email = "email Field can not be empty";
+}
+
+// subject
+const subjectField = document.getElementById("subjectField").value;
+
+
+if (subjectField == "") {
+  errors.password = "subject field can not be empty";
+}
+
+
+
+// radio
+let gender = false;
+
+this.querySelectorAll('[name="gender"]').forEach((item) => {
+  if (item.checked) {
+    gender = true;
+  }
+});
+
+if (!gender) {
+  errors.gender = "Please select your gender";
+}
+
+// message
+const MessageFilde = document.getElementById("MessageFilde").checked;
+if (!checkBox) {
+  errors.check = "You must Message our terms and conditions";
+}
+
+console.log(errors);
+
+// submit
+
+formElement.addEventListener('submit', event => {
+  event.preventDefault();
+  validateAllFormGroups(formElement);
+})
+// validateForm('#registrationForm');
+
+
+// cookies
+const getCookie = (name) => {
+  const value = " " + document.cookie;
+  console.log("value", `==${value}==`);
+  const parts = value.split(" " + name + "=");
+  return parts.length < 2 ? undefined : parts.pop().split(";").shift();
+};
+
+const setCookie = function (name, value, expiryDays, domain, path, secure) {
+  const exdate = new Date();
+  exdate.setHours(
+    exdate.getHours() +
+    (typeof expiryDays !== "number" ? 365 : expiryDays) * 24
+  );
+  document.cookie =
+    name +
+    "=" +
+    value +
+    ";expires=" +
+    exdate.toUTCString() +
+    ";path=" +
+    (path || "/") +
+    (domain ? ";domain=" + domain : "") +
+    (secure ? ";secure" : "");
+};
+
+// npm
+
+
+
+
+
+
+
+
